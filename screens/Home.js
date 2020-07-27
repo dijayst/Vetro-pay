7fb57b5dae8e93013452aa272cfcf3f25cabb5b5
Olamigoke Philip <olamigokayphils@gmail.com>
2020-07-27 23:12:29 +0100

    - Enhanced transaction notification - Link mobile account to Vetropay Business

--- screens/Home.js
+++ screens/Home.js
@@ -68,9 +68,35 @@ export default function Home({ navigation }) {
 
   useEffect(() => {
     let isMounted = true;
-    registerForPushNotificationsAsync().then((token) => {
-      setExpoPushToken(token);
-    });
+    (async () => {
+      let token;
+      if (Constants.isDevice) {
+        const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+        let finalStatus = existingStatus;
+        if (existingStatus !== "granted") {
+          const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+          finalStatus = status;
+        }
+
+        if (finalStatus !== "granted") {
+          alert("Failed to get push token for push notification!");
+          return;
+        }
+        token = (await Notifications.getExpoPushTokenAsync()).data;
+        setExpoPushToken(token);
+      } else {
+        alert("Must use physical device for Push Notifications");
+      }
+    })();
+
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
 
     notificationListener.current = Notifications.addNotificationReceivedListener((notification) => {
       setNotification(notification);
@@ -95,36 +121,6 @@ export default function Home({ navigation }) {
       }
     }
   }, [expoPushToken]);
-
-  async function registerForPushNotificationsAsync() {
-    let token;
-    if (Constants.isDevice) {
-      const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
-      let finalStatus = existingStatus;
-      if (existingStatus !== "granted") {
-        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-        finalStatus = status;
-      }
-      if (finalStatus !== "granted") {
-        alert("Failed to get push token for push notification!");
-        return;
-      }
-      token = (await Notifications.getExpoPushTokenAsync()).data;
-    } else {
-      alert("Must use physical device for Push Notifications");
-    }
-
-    if (Platform.OS === "android") {
-      Notifications.setNotificationChannelAsync("default", {
-        name: "default",
-        importance: Notifications.AndroidImportance.MAX,
-        vibrationPattern: [0, 250, 250, 250],
-        lightColor: "#FF231F7C",
-      });
-    }
-
-    return token;
-  }
 
   useEffect(() => {
     /**NAVIGATIOON */
