10d3ea14d83d1aa6ba653c41686bea6b94596db6
Olamigoke Philip <olamigokayphils@gmail.com>
2022-07-06 05:18:31 +0100

    update transaction pin flow

--- containers/authentication/reducer.js
+++ containers/authentication/reducer.js
@@ -1,5 +1,5 @@
 import localStorage from "react-native-sync-localstorage";
-import { USER_LOADING, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT_SUCCESS } from "../rootAction/types";
+import { USER_LOADING, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT_SUCCESS, PUSH_ACTIVITY_VERIFICATION_TOKEN } from "../rootAction/types";
 
 const initialState = {
   token: localStorage.getItem("token"),
@@ -7,6 +7,7 @@ const initialState = {
   isLoading: false,
   user: null,
   dateTime: new Date(),
+  activityToken: [],
 };
 
 export default function (state = initialState, action) {
@@ -47,6 +48,12 @@ export default function (state = initialState, action) {
         isAuthenticated: false,
         isLoading: false,
         dateTime: new Date(),
+      };
+
+    case PUSH_ACTIVITY_VERIFICATION_TOKEN:
+      return {
+        ...state,
+        activityToken: [...state.activityToken, action.payload],
       };
 
     default:
